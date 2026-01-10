e0af7f3e91ace692b4ca227fc07a9dc9c4b4cc52
WeiHanChen <weihanchen0725@gmail.com>
2026-01-09 18:38:17 -0800

    feat: update NavBar component to include navigation links

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 const NavBar = () => {
     return (
-        <>NavBar</>
+        <>
+        NavBar
+        <Link href='/'>Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/tools">Tools</Link>
+        <Link href="/news">News</Link>
+        <Link href="/contact">Contact</Link>
+        </>
     )
 }
 
